ae84e81118f44d9289917eef2c61eaa7807e7740
Coxtrodomus <111580381+Coxtrodomus@users.noreply.github.com>
2022-08-19 15:57:33 -0400

    Update header.php

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
-	<a class="navbar-brand" href="home.php">Home</a>
+	<a class="navbar-brand" href="index.html">Home</a>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 	</button>
